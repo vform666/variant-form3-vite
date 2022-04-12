f9e4538f10ab6fdd426631264082bde02c3712f6
vdpAdmin <vdpadmin@163.com>
2022-04-12 18:21:50 +0800

    补充遗漏的refMixinDesign.js文件。

---
+++ src/components/form-designer/refMixinDesign.js
@@ -0,0 +1,28 @@
+export default {
+  methods: {
+    initRefList() {
+      if ((this.refList !== null) && !!this.widget.options.name) {
+        this.refList[this.widget.options.name] = this
+      }
+    },
+
+    getWidgetRef(widgetName, showError) {
+      let foundRef = this.refList[widgetName]
+      if (!foundRef && !!showError) {
+        this.$message.error(this.i18nt('render.hint.refNotFound') + widgetName)
+      }
+      return foundRef
+    },
+
+    /* 该方法用于组件重名检查！！ */
+    registerToRefList(oldRefName) {
+      if ((this.refList !== null) && !!this.widget.options.name) {
+        if (!!oldRefName) {
+          delete this.refList[oldRefName]
+        }
+        this.refList[this.widget.options.name] = this
+      }
+    },
+
+  }
+}
